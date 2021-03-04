c1fc6b7ae6944abeb3ccb0c99919f4a2af20bcc7
luca <beccibrian@gmail.com>
2021-03-04 12:08:43 -0300

    chore(@all): Config added for db, port and morgan state

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export default {
     PORT: process.env.PORT || 4000,
     DB_URI: process.env.DB_URI || "undefined",
+    DEV: process.env.DEV || true,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,31 @@
-console.log("hello world");
+import express, { Application } from "express";
+import morgan from "morgan";
+import config from "./config";
+
+class Server {
+    private _app: Application;
+    constructor() {
+        this._app = express();
+
+        this.initDatabase;
+        this.initConfig;
+        this.initRoutes;
+    }
+    private initDatabase() {
+        console.log("database");
+    }
+    private initConfig() {
+        this._app.use(morgan(config.DEV ? "dev" : "combined"));
+        console.log("config");
+    }
+    private initRoutes() {
+        console.log("routes");
+    }
+
+    public run() {
+        this._app.listen(config.PORT, () => {
+            console.log("Server on port:", config.PORT);
+        });
+    }
+}
+export default Server;

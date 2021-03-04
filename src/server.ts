f3d3416b48114c4b071208a53c57a364069a71f2
luca <beccibrian@gmail.com>
2021-03-04 13:19:01 -0300

    chore(@all): cookie sess and routes for github and google added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Application, IRouter } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import cookieSession from "cookie-session";
+import passport from "passport";
 
 import config from "./config";
 import indexRouter from "./routes/index.routes";
@@ -19,14 +21,25 @@ class Server {
         console.log("database");
     }
     private initConfig() {
-        this._app.use(morgan(config.DEV ? "dev" : "combined"));
-        this._app.use(express.json());
-        this._app.use(express.urlencoded({ extended: false }));
         this._app.use(
             cors({
                 credentials: true,
             })
         );
+        this._app.use(morgan(config.DEV ? "dev" : "combined"));
+        this._app.use(express.json());
+        this._app.use(express.urlencoded({ extended: false }));
+
+        //cookie
+        this._app.use(
+            cookieSession({
+                name: "auth",
+                keys: ["key1, key2"],
+                maxAge: 60 * 60 * 60,
+            })
+        );
+        this._app.use(passport.initialize());
+        this._app.use(passport.session());
     }
     private initRoutes() {
         this._app.use(this._indexRouter);
